0475199a05c106711b03d487cd876855159acc88
liuhongfei00 <hongfei.liu@mail.mcgill.ca>
2026-05-30 15:57:10 -0400

    Implemented InventoryDao and RecipeDao

---
+++ src/main/java/com/jc451/team1/DAO/InventoryItemDao.java
@@ -0,0 +1,18 @@
+package com.jc451.team1.DAO;
+
+import com.jc451.team1.DTO.InventoryItem;
+
+import java.util.List;
+
+public interface InventoryItemDao {
+
+    InventoryItem addInventoryItem(InventoryItem inventoryItem);
+
+    List<InventoryItem> getAllInventoryItems();
+
+    InventoryItem findInventoryItemById(int id);
+
+    void updateInventoryItem(InventoryItem inventoryItem);
+
+    void removeInventoryItem(int id);
+}
