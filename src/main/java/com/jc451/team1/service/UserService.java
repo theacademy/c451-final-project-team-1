c201e19c3cf145ae09c4b9a123e225f298406d50
Zachary Therrien <ZTherrien.work@gmail.com>
2026-05-31 13:53:16 -0400

    User service layer issues fixed

--- src/main/java/com/jc451/team1/service/UserService.java
+++ src/main/java/com/jc451/team1/service/UserService.java
@@ -1,17 +1,32 @@
 package com.jc451.team1.service;
 
+import com.jc451.team1.dto.Recipe;
 import com.jc451.team1.dto.User;
 
 import java.util.List;
 
 public interface UserService {
-    void createNewUser(User user);
+    User createUser(User user);
 
-    User getUser(int id);
+    User getUserById(int id);
+
+    User getUserByUsername(String username); // for login
 
     List<User> getUsersPerHousehold(int householdId);
 
     List<User> getAllUsers();
 
     void updateUser(User user);
+
+    void addIntolerance(User user, int intoleranceId);
+
+    void deleteIntolerance(User user, String intolerance);
+
+    void addDietaryRestriction(User user,  int dietId);
+
+    void deleteDietaryRestriction(User user, String diet);
+
+    void addRecipe(User user, Recipe recipe);
+
+    void deleteRecipe(User user, Recipe recipe);
 }
