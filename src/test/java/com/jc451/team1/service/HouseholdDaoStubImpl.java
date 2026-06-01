95c07e4be5f0fdefbbe65603bbea5aab423da75f
Zachary Therrien <ZTherrien.work@gmail.com>
2026-06-01 01:48:17 -0400

    Stub Implementations created

---
+++ src/test/java/com/jc451/team1/service/HouseholdDaoStubImpl.java
@@ -0,0 +1,55 @@
+package com.jc451.team1.service;
+
+import com.jc451.team1.dao.HouseholdDao;
+import com.jc451.team1.dto.Household;
+import com.jc451.team1.dto.InventoryItem;
+import com.jc451.team1.dto.User;
+
+import java.util.List;
+
+public class HouseholdDaoStubImpl implements HouseholdDao {
+
+    public Household household;
+
+    public HouseholdDaoStubImpl(){
+        household.setHouseholdId(1);
+        household.setHouseholdName("TestName");
+        household.setAddress("testAddress");
+        household.setCode("TestCode");
+    }
+
+    @Override
+    public Household createHousehold(Household household) {
+        return null;
+    }
+
+    @Override
+    public Household getHouseholdById(int id) {
+        return null;
+    }
+
+    @Override
+    public Household getHouseholdByCode(String householdCode) {
+        return null;
+    }
+
+    @Override
+    public void addUserToHousehold(int userId, int householdId) {
+
+    }
+
+    @Override
+    public void removeUserFromHousehold(int userId, int householdId) {
+
+    }
+
+    @Override
+    public List<InventoryItem> getInventoryItems(int householdId) {
+        return List.of();
+    }
+
+    @Override
+    public List<User> getAllUsers(int householdId) {
+        return List.of();
+    }
+}
