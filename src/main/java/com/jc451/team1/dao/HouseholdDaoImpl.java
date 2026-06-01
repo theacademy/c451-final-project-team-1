08557a9473681c845fc518968f698a6dfe70410a
nyaandan <dominiqueassi99@gmail.com>
2026-06-01 00:56:51 -0400

    Reviewed and adjusted the data access from all 4 DAOs

--- src/main/java/com/jc451/team1/dao/HouseholdDaoImpl.java
+++ src/main/java/com/jc451/team1/dao/HouseholdDaoImpl.java
@@ -4,25 +4,29 @@ import com.jc451.team1.dao.mappers.*;
 import com.jc451.team1.dto.Household;
 import com.jc451.team1.dto.InventoryItem;
 import com.jc451.team1.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Repository;
 
 import java.sql.*;
 import java.util.List;
 
+@Repository
 public class HouseholdDaoImpl implements HouseholdDao {
 
     private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public HouseholdDaoImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public Household createHousehold(Household household) {
-        final String INSERT_HOUSEHOLD = "INSERT INTO households(code, nickname, address) " +
-                "VALUES(?, ?, ?)";
+        final String INSERT_HOUSEHOLD = """
+                INSERT INTO households(code, nickname, address)
+                VALUES(?, ?, ?)""";
 
         GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
 
@@ -36,8 +40,10 @@ public class HouseholdDaoImpl implements HouseholdDao {
             return statement;
         }, keyHolder);
 
-        household.setHouseholdId(keyHolder.getKey().intValue());
-
+        Number key = keyHolder.getKey();
+        if(key != null){
+            household.setHouseholdId(key.intValue());
+        }
         return household;
     }
 
@@ -45,14 +51,13 @@ public class HouseholdDaoImpl implements HouseholdDao {
     public Household getHouseholdById(int id) {
         final String SELECT_HOUSEHOLD_BY_ID = "SELECT * FROM households WHERE id = ?";
 
-        Household household = jdbcTemplate.queryForObject(SELECT_HOUSEHOLD_BY_ID, new HouseholdMapper(), id);
+        Household household = jdbcTemplate.queryForObject(
+                SELECT_HOUSEHOLD_BY_ID,
+                new HouseholdMapper(), id);
 
-        if (household == null){
-            return null;
+        if (household != null) {
+            household.setUsers(getAllUsers(id));
         }
-
-        // If the household exists, get its users
-        household.setUsers(this.getAllUsers(id));
 
         return household;
     }
@@ -61,33 +66,22 @@ public class HouseholdDaoImpl implements HouseholdDao {
     public Household getHouseholdByCode(String householdCode) {
         final String SELECT_HOUSEHOLD_BY_CODE = "SELECT * FROM households WHERE code = ?";
 
-        Household household = jdbcTemplate.queryForObject(SELECT_HOUSEHOLD_BY_CODE, new HouseholdMapper(), householdCode);
+        Household household = jdbcTemplate.queryForObject(
+                SELECT_HOUSEHOLD_BY_CODE, new HouseholdMapper(), householdCode);
 
-        if (household == null){
-            return null;
+        if (household != null) {
+            household.setUsers(this.getAllUsers(household.getHouseholdId()));
         }
-
-        // If the household exists, get its users
-        household.setUsers(this.getAllUsers(household.getHouseholdId()));
 
         return household;
     }
 
     @Override
     public void addUserToHousehold(int userId, int householdId) {
-        final String INSERT_HOUSEHOLD = "INSERT INTO household_memberships(user_id, household_id) " +
-                "VALUES(?, ?)";
-
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update((Connection conn) -> {
-            PreparedStatement statement = conn.prepareStatement(
-                    INSERT_HOUSEHOLD,
-                    Statement.RETURN_GENERATED_KEYS);
-            statement.setInt(1, userId);
-            statement.setInt(2, householdId);
-            return statement;
-        });
+        final String INSERT_HOUSEHOLD = """
+                INSERT INTO household_memberships(user_id, household_id)
+                VALUES(?, ?)""";
+        jdbcTemplate.update(INSERT_HOUSEHOLD, userId, householdId);
     }
 
     @Override
@@ -107,26 +101,46 @@ public class HouseholdDaoImpl implements HouseholdDao {
     @Override
     public List<User> getAllUsers(int householdId) {
         // Get users who are a part of the household
-        final String SELECT_HOUSEHOLD_USERS = "SELECT * FROM household_memberships " +
-                "JOIN users ON users.id = household_memberships.user_id " +
-                "WHERE household_memberships.household_id = ?";
+        final String SELECT_HOUSEHOLD_USERS = """
+                SELECT *
+                FROM household_memberships
+                JOIN users ON users.id = household_memberships.user_id
+                WHERE household_memberships.household_id = ?""";
         List<User> users = jdbcTemplate.query(SELECT_HOUSEHOLD_USERS, new UserMapper(), householdId);
 
-        // Get the user's Dietary Restrictions, Intolerances, and Recipes
-        final String SELECT_USER_DIETARY_RESTRICTIONS_BY_ID = "SELECT diets.name FROM users " +
-                "JOIN dietary_restrictions ON dietary_restrictions.user_id = ? " +
-                "JOIN diets ON dietary_restrictions.diet_id = diets.id";
-        final String SELECT_USER_INTOLERANCE_RESTRICTIONS_BY_ID = "SELECT items.name FROM users " +
-                "JOIN intolerances ON intolerances.user_id = ? " +
-                "JOIN items ON intolerances.item_id = items.id";
-        final String SELECT_SAVED_RECIPES_BY_USER_ID = "SELECT * FROM users " +
-                "JOIN saved_recipes ON saved_recipes.user_id = ? " +
-                "JOIN recipes ON recipes.id = saved_recipes.recipe_id";
+        // Queries for the user's dietary restrictions, intolerances, and saved recipes
+        final String SELECT_USER_DIETARY_RESTRICTIONS_BY_ID = """
+                SELECT diets.name
+                FROM users
+                JOIN dietary_restrictions ON users.id = dietary_restrictions.user_id
+                JOIN diets ON dietary_restrictions.diet_id = diets.id""";
 
-        for (User user : users){
-            user.setDietaryRestrictions(jdbcTemplate.query(SELECT_USER_DIETARY_RESTRICTIONS_BY_ID, new DietsMapper(), user.getUserId()));
-            user.setIntolerances(jdbcTemplate.query(SELECT_USER_INTOLERANCE_RESTRICTIONS_BY_ID, new IntoleranceMapper(), user.getUserId()));
-            user.setRecipes(jdbcTemplate.query(SELECT_SAVED_RECIPES_BY_USER_ID, new RecipeMapper(), user.getUserId()));
+        final String SELECT_USER_INTOLERANCE_RESTRICTIONS_BY_ID = """
+                SELECT items.name
+                FROM users
+                JOIN intolerances ON users.id = intolerances.user_id
+                JOIN items ON intolerances.item_id = items.id""";
+
+        final String SELECT_SAVED_RECIPES_BY_USER_ID = """
+                SELECT *
+                FROM users
+                JOIN saved_recipes ON users.id = saved_recipes.user_id
+                JOIN recipes ON saved_recipes.recipe_id = recipes.id""";
+
+        for (User user : users) {
+            user.setDietaryRestrictions(jdbcTemplate.query(
+                    SELECT_USER_DIETARY_RESTRICTIONS_BY_ID,
+                    (rs, rowNum) -> rs.getString("name"),
+                    user.getUserId()));
+
+            user.setIntolerances(jdbcTemplate.query(
+                    SELECT_USER_INTOLERANCE_RESTRICTIONS_BY_ID,
+                    (rs, rowNum) -> rs.getString("name"),
+                    user.getUserId()));
+
+            user.setRecipes(jdbcTemplate.query(
+                    SELECT_SAVED_RECIPES_BY_USER_ID,
+                    new RecipeMapper(), user.getUserId()));
         }
 
         return users;
--- src/main/java/com/jc451/team1/dao/mappers/DietsMapper.java
+++ src/main/java/com/jc451/team1/dao/mappers/DietsMapper.java
@@ -5,13 +5,10 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+// Used lambdas instead where it's relevant
 public class DietsMapper implements RowMapper<String> {
     @Override
     public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-        String diet = "";
-
-        diet = rs.getString("name");
-
-        return diet;
+        return rs.getString("name");
     }
 }
